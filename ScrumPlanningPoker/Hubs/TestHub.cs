aa571115777f06acc5030dfd8450eb32fae7a884
LaM0uette <LaM0uette.dev@gmail.com>
2023-11-23 21:52:40 +0100

    re structure project

---
+++ ScrumPlanningPoker/Hubs/TestHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ScrumPlanningPoker.Hubs;
+
+public class TestHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        Console.WriteLine($"SendMessage: {user} - {message}");
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
